b01dd092dd93bec673e6ee983cf3a6ceb6ca3f5b
stonecoldshree <shreesujal1405@gmail.com>
2026-04-14 19:25:14 +0530

    deploy fix2

--- middleware.js
+++ middleware.js
@@ -56,8 +56,6 @@ export default createMiddleware(aj, clerk);
 export const config = {
   matcher: [
 
-  "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-
-  "/(api|trpc)(?!/webhooks/clerk)(.*)"]
+  "/((?!api/webhooks/clerk|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)"]
 
 };
